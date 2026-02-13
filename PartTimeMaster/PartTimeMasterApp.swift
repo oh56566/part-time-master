36d88b81c0b5cd7e122bdf95a16c6352f34cd62f
Depths <oh56566@naver.com>
2026-02-13 11:34:24 +0900

    Initial Commit

---
+++ PartTimeMaster/PartTimeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartTimeMasterApp.swift
+//  PartTimeMaster
+//
+//  Created by 오대현 on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct PartTimeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
